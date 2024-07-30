da1c88ad0a7289e807c55128d6f6f17e7f3cee11
sudipX0 <sudippoudel@sd.taylors.edu.my>
2024-07-30 22:27:26 +0545

    Alert Button on Register Page

--- app/signup.jsx
+++ app/signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
 import { useNavigation } from 'expo-router';
 
 export default function SignUpScreen() {
