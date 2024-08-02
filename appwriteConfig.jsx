0d4f7050e9e030bce054a5c5195cd522c5c23256
sudipX0 <sudippoudel@sd.taylors.edu.my>
2024-08-02 13:32:37 +0545

    Removed appwrite config

--- appwriteConfig.jsx
+++
@@ -1,9 +0,0 @@
-import { Client, Account, Databases, ID } from 'appwrite';
-
-// Initialize Appwrite client with API key
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
-    .setProject('raktax0') // Your Appwrite project ID
-
-export const account = new Account(client);
-export const databases = new Databases(client);
